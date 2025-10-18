aa01198260ef9d2711633dd4193a83e8e901f1a9
Malandro <232688017+ser-malandro@users.noreply.github.com>
2025-10-18 13:33:26 -0500

    finished repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
+    let i = (num);
+    let string = ""
+    for (i = 0; i < (num); i++){
+        string += word;
+    }
+    return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
