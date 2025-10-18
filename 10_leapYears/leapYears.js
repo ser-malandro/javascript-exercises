9e0b0acaad65226758d6a171e72b3a2693eb5f14
Malandro <232688017+ser-malandro@users.noreply.github.com>
2025-10-18 14:34:31 -0500

    finished leapYears question

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isCentury = year % 100 === 0;
+    const isLeap = year % 4 === 0;
+    const isYearDivisibleByFourH = year % 400 === 0;
 
+    if (isLeap && (!isCentury || isYearDivisibleByFourH)
+    ){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
