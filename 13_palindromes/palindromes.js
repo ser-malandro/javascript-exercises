a584fa3728cbee4a3d578609d539083d4cae8bee
Malandro <232688017+ser-malandro@users.noreply.github.com>
2025-10-18 13:09:42 -0500

    completed object basics assignment

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanums = "abcdefghijklmnopqrstuvxwxyz123456789";
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanums.includes(character))
+    .join('');
+    
+    const reverse = cleanedString.split('').reverse().join('');
 
+    return cleanedString === reverse;
 };
 
 // Do not edit below this line
