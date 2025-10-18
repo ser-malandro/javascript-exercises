37fba051d165d6d42f9feba688addbdecf010ebd
Malandro <232688017+ser-malandro@users.noreply.github.com>
2025-10-18 14:56:24 -0500

    finished tempConversion question

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (temp === 32){
+    return 0;
+  }
+  else{
+    return (Math.round(((temp - 32)) * (5/9) * 10) / 10);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(((temp * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
