61989cc19f1a30e50a53efc199c79b44f7109024
Malandro <232688017+ser-malandro@users.noreply.github.com>
2025-10-18 14:14:28 -0500

    finished removeFromArrayQuestion

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    return array.filter(answer => !arg.includes(answer));
 };
 
 // Do not edit below this line
